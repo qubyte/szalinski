ec07b27e184eb98a4a9219ceb51e193453b8abd1
Mark S. Everitt <mark.s.everitt@gmail.com>
2016-11-05 12:04:00 +0000

    Slightly streamlines the index module.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const app = require('./lib/app');
 const http = require('http');
+const app = require('./lib/app');
 const logger = require('./lib/logger');
 const config = require('./lib/config');
 
@@ -9,23 +9,20 @@ const server = http.createServer(app.requestHandler).listen(config.appPort, () =
   logger.info(`listening on ${config.appPort}`);
 });
 
-function attemptGracefulExit() {
+function attemptGracefulExit(err, reason) {
+  if (err) {
+    logger.error({ err }, reason);
+  } else {
+    logger.warn(reason);
+  }
+
   server.close(() => process.exit(0));
   setTimeout(() => process.exit(1), 1000);
 }
 
-process.on('unhandledRejection', (err) => {
-  logger.error({ err }, 'Unhandled Rejection.');
-  attemptGracefulExit();
-});
+// There should be no unhandled promise rejections. Shut down if one occurs.
+process.on('unhandledRejection', err => attemptGracefulExit(err, 'Unhandled Rejection.'));
 
 // Explicit handling for signals from docker.
-process.on('SIGINT', () => {
-  logger.warn('Process received SIGINT. Quitting...');
-  attemptGracefulExit();
-});
-
-process.on('SIGTERM', () => {
-  logger.warn('Process received SIGTERM. Quitting...');
-  attemptGracefulExit();
-});
+process.on('SIGINT', () => attemptGracefulExit(null, 'Process received SIGINT. Quitting...'));
+process.on('SIGTERM', () => attemptGracefulExit(null, 'Process received SIGTERM. Quitting...'));
